1e292897b617704e26340d1e15b2332093cb1e85
aleprado <ale.nesti@gmail.com>
2025-06-30 15:51:46 -0300

    Ordenar recorrido por orden

--- src/frontend/app/mapa.js
+++ src/frontend/app/mapa.js
@@ -1,5 +1,5 @@
 import { db } from './config.js'
-import { doc, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
+import { doc, collection, getDocs, query, orderBy } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
 const parametros = new URLSearchParams(window.location.search)
 const ruta = parametros.get('ruta')
 const mapa = L.map('mapa').setView([0,0],13)
@@ -7,7 +7,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{}).addTo(mapa)
 async function dibujar(){
     const refRuta = doc(db,'Rutas',ruta)
     const refRecorrido = collection(refRuta,'RutaRecorrido')
-    const snap = await getDocs(refRecorrido)
+    const consulta = query(refRecorrido, orderBy('Orden'))
+    const snap = await getDocs(consulta)
     const puntos = snap.docs.map(d=>[d.data().latitud,d.data().longitud])
     if(puntos.length){
         mapa.setView(puntos[0],13)
