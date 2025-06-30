2921d53bc3cf91cbf7ba5fd1315367b62fe86da5
aleprado <ale.nesti@gmail.com>
2025-06-30 17:33:57 -0300

    Corrige lectura de puntos para ruta

--- src/frontend/app/mapa.js
+++ src/frontend/app/mapa.js
@@ -1,5 +1,5 @@
 import { db } from './config.js'
-import { doc, collection, getDocs, query, orderBy } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
+import { doc, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
 const parametros = new URLSearchParams(window.location.search)
 const ruta = parametros.get('ruta')
 const mapa = L.map('mapa').setView([0,0],13)
@@ -7,9 +7,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{}).addTo(mapa)
 async function dibujar(){
     const refRuta = doc(db,'Rutas',ruta)
     const refRecorrido = collection(refRuta,'RutaRecorrido')
-    const consulta = query(refRecorrido, orderBy('Orden'))
-    const snap = await getDocs(consulta)
-    const puntos = snap.docs.map(d=>[d.data().latitud,d.data().longitud])
+    const resultado = await getDocs(refRecorrido)
+    const puntos = resultado.docs
+        .sort((a,b)=>parseInt(a.id)-parseInt(b.id))
+        .map(d=>[d.data().latitud,d.data().longitud])
     if(puntos.length){
         mapa.setView(puntos[0],13)
         L.polyline(puntos,{color:'green'}).addTo(mapa)
